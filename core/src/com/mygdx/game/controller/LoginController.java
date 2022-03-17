a87e2679d09d28f6f48ab56fc766ff40a5cddf99
Erlend <erlend_haakegaard@hotmail.com>
2022-03-17 15:08:28 +0100

    login view og controller

---
+++ core/src/com/mygdx/game/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.mygdx.game.controller;
+
+import com.mygdx.game.view.LoginView;
+
+public class LoginController extends Controller<LoginView>{
+    private static LoginController instance = null;
+
+    private LoginController(ControllerManager controllerManager){
+        super(controllerManager, LoginView.getInstance());
+    }
+
+    public static final LoginController getInstance(ControllerManager controllerManager){
+        if (instance == null){
+            return new LoginController(controllerManager);
+        }
+        return instance;
+    }
+}
---
+++ core/src/com/mygdx/game/view/LoginView.java
@@ -0,0 +1,35 @@
+package com.mygdx.game.view;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.StripaSurvivor;
+
+import jdk.tools.jaotc.binformat.macho.MachOSegment;
+
+public class LoginView extends View{
+    private static LoginView instance = null;
+
+    private LoginView(){
+        super();
+    }
+
+    public static final LoginView getInstance(){
+        if (instance == null){
+            return new LoginView();
+        }
+        return instance;
+    }
+
+    @Override
+    public void handleInput() { }
+
+    @Override
+    public void update(float dt) { }
+
+    @Override
+    public void render(SpriteBatch sb) {
+    }
+
+    @Override
+    public void dispose() { }
+
+}
