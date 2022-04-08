f7423c92dffd6d9547c7d07d4b213614451009af
Erlend <erlend_haakegaard@hotmail.com>
2022-04-08 17:08:44 +0200

    Model start

--- core/src/com/mygdx/game/controller/GameController.java
+++ core/src/com/mygdx/game/controller/GameController.java
@@ -1,13 +1,21 @@
 package com.mygdx.game.controller;
 
+import com.mygdx.game.model.Model;
+import com.mygdx.game.model.PlayerModel;
 import com.mygdx.game.view.game.GameView;
 import com.mygdx.game.view.leaderboard.LeaderboardView;
+
+import java.util.ArrayList;
 
 public class GameController extends Controller<GameView>{
     private static GameController instance = null;
 
+    private ArrayList<Model> models;
+    private PlayerModel playerModel;
+
     private GameController(ControllerManager controllerManager){
         super(controllerManager, GameView.getInstance());
+        models = new ArrayList<>();
     }
 
     public static final GameController getInstance(ControllerManager controllerManager){
@@ -19,6 +27,14 @@ public class GameController extends Controller<GameView>{
 
     @Override
     public void update(float dt) {
+        playerModel.update(dt);
+        for (Model model : models){
+            model.update(dt);
+            if(playerModel.collides(model.getCollisionBox())){
+                model.interact(playerModel);
+            }
+        }
+        //render
 
     }
 }
