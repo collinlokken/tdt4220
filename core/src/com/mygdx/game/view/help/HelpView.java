823e1a547639b6977fc9007dd069091733cc2922
Kristian Hansen <kirinsen97@gmail.com>
2022-04-24 18:37:21 +0200

    added more pics

--- core/src/com/mygdx/game/view/help/HelpView.java
+++ core/src/com/mygdx/game/view/help/HelpView.java
@@ -1,34 +1,62 @@
 package com.mygdx.game.view.help;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.mygdx.game.StripaSurvivor;
+import com.mygdx.game.controller.GameController;
 import com.mygdx.game.controller.ControllerManager;
 import com.mygdx.game.controller.HelpController;
 import com.mygdx.game.controller.MainMenuController;
 import com.mygdx.game.view.View;
 
+import java.util.ArrayList;
+
 public class HelpView extends View<HelpController> {
     private static HelpView instance = null;
 
+
+
     private HelpView(){
-        Image background = new Image(new TextureRegionDrawable(new TextureRegion(new Texture("tutorialsBiggerText.png"))));
-        background.setPosition(0, 0);
-        background.setSize(getCamera().viewportWidth, getCamera().viewportHeight);
-        background.addListener(new ClickListener(){
+        Image firstSlide = new Image(new TextureRegionDrawable(new TextureRegion(new Texture("tutorialsBiggerText.png"))));
+        Image secondSlide = new Image(new TextureRegionDrawable(new TextureRegion(new Texture("tutorialsBiggerText.png"))));
+        Image thirdSlide = new Image(new TextureRegionDrawable(new TextureRegion(new Texture("tutorialsBiggerText.png"))));
+
+        ArrayList<Image> images = new ArrayList<Image>();
+        images.add(firstSlide);
+        images.add(secondSlide);
+        images.add(thirdSlide);
+        firstSlide.setPosition(0, 0);
+        firstSlide.setSize(getCamera().viewportWidth, getCamera().viewportHeight);
+        firstSlide.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y){
                 super.clicked(event, x, y);
                 ControllerManager.getInstance().set(MainMenuController.getInstance());
             }
         });
-        this.addActor(background);
+        this.addActor(firstSlide);
+
+        // nextButton
+        ImageButton nextButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("heloNextButton2.png"))));
+        nextButton.setSize(64, 64);
+        nextButton.setPosition((float) (getCamera().viewportWidth*0.93-nextButton.getWidth()/2), (float) (getCamera().viewportHeight*0.50));
+        nextButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+
+            }
+        });
+        this.addActor(nextButton);
 
     }
 
@@ -38,4 +66,7 @@ public class HelpView extends View<HelpController> {
         }
         return instance;
     }
+    //public void nextSlide
+
+
 }
