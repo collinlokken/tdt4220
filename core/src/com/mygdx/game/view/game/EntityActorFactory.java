cdac0c90d78f2faecfb1571f3c1db07f0eaf8c2b
Lars Sørensen <larssorensen98@hotmail.com>
2022-05-02 11:52:58 +0200

    Slettet ProtectionAgainstActor

--- core/src/com/mygdx/game/view/game/EntityActorFactory.java
+++ core/src/com/mygdx/game/view/game/EntityActorFactory.java
@@ -19,7 +19,6 @@ public class EntityActorFactory
 
     }
     public EntityActor createActor (Entity entity) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-
         //Using Reflection to dynamically create new actor by using the convention that all actor classes have the name of format: <EntityName>Actor
         Class<? extends EntityActor> actorClass = (Class<? extends EntityActor>) Class.forName("com.mygdx.game.view.game.actors.".concat(entity.getClass().getSimpleName()).concat("Actor"));
         return actorClass.getConstructor(entity.getClass()).newInstance(entity);
--- core/src/com/mygdx/game/view/game/actors/PowerupStatusActor.java
+++
@@ -1,8 +0,0 @@
-package com.mygdx.game.view.game.actors;
-
-import com.badlogic.gdx.scenes.scene2d.Actor;
-
-public class PowerupStatusActor extends Actor
-{
-
-}
